8a6ef971338e3e2d78431854e4e319dc8c5be452
My Name <srivastavaadi129@gmail.com>
2024-01-13 18:38:10 +0530

    fixed

--- middlewares/error.js
+++ middlewares/error.js
@@ -5,14 +5,26 @@ class ErrorHandler extends Error {
   }
 }
 
-export const errorMiddleware = (err, req, res, next) => {
-  err.message = err.message || "Internal Server Error";
-  err.statusCode = err.statusCode || 500;
+// export const errorMiddleware = (err, req, res, next) => {
+//   err.message = err.message || "Internal Server Error";
+//   err.statusCode = err.statusCode || 500;
 
-  return res.status(err.statusCode).json({
+//   return res.status(err.statusCode).json({
+//     success: false,
+//     message: err.message,
+//   });
+// };
+
+export const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
     success: false,
     message: err.message,
+    stack: process.env.NODE_ENV === 'Production' ? null : err.stack,
   });
 };
+
+
+
 
 export default ErrorHandler;
